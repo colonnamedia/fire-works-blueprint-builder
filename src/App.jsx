ed0a9d6fa513dde2de95293705c40ebe4dba6dad
colonnamedia <colonnamedia@gmail.com>
2026-08-02 10:31:20 -0400

    Add routes for AdvertisingQuestionnaire and AdvertisingResults

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Toaster } from "@/components/ui/toaster";
 import Landing from './pages/Landing';
 import Questionnaire from './pages/Questionnaire';
 import Results from './pages/Results';
+import AdvertisingQuestionnaire from './pages/AdvertisingQuestionnaire';
+import AdvertisingResults from './pages/AdvertisingResults';
 
 function App() {
   return (
@@ -11,10 +13,11 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/questionnaire" element={<Questionnaire />} />
         <Route path="/results" element={<Results />} />
+        <Route path="/advertising" element={<AdvertisingQuestionnaire />} />
+        <Route path="/advertising-results" element={<AdvertisingResults />} />
       </Routes>
       <Toaster />
     </Router>
   );
 }
-
 export default App;
